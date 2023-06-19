03532fc747366d500712b8ac2e528e8bb862b49c
clarkjoao <clark.joao@gmail.com>
2023-06-19 10:03:30 -0300

    add header

--- blank-plataform/src/App.tsx
+++ blank-plataform/src/App.tsx
@@ -3,12 +3,11 @@ import "./App.css";
 // @ts-ignore
 import TodoList from "TodoListApp/TodoList";
 function App() {
-  const [path, setPath] = useState('');
+  const [showOpt, setShowOpt] = useState<boolean>(false);
   const [show1, setShow1] = useState(false);
   const [show2, setShow2] = useState(false);
 
   useEffect(() => {
-    console.log(window.location.pathname)
     if (window.location.pathname === '/app1') {
       setShow1(true)
     }
@@ -16,25 +15,36 @@ function App() {
       setShow2(true)
     }
 
-    setPath(window.location.pathname)
-  }, [])
+    if (window.location.pathname === '/') {
+      setShowOpt(true)
+    }
+
+  }, [window.location.pathname])
 
   return (
     <>
-      <h1>Host Application</h1>
-      {!path.length ? <div>
-        <button onClick={() => setShow1(!show1)}>Show App 1: {show1.toString()}</button>
-        <button onClick={() => setShow2(!show2)}>Show App 2: {show2.toString()}</button>
-      </div> : null}
+      <header>
+        <h1>Blank Plataform: {window.location.pathname}</h1>
+        {!showOpt ? <a href="/">Back to Home</a> : null}
+      </header>
+
+      {showOpt ?
+        (<>
+          <button onClick={() => setShow1(!show1)}>Show App 1: {show1.toString()}</button>
+          <button onClick={() => setShow2(!show2)}>Show App 2: {show2.toString()}</button>
+
+        </>)
+        : null
+      }
       <div className="App">
-        {show1 || path === '/app1' ?
+        {show1 ?
           <div className="card">
-            <TodoList title="App 1"/>
+            <TodoList title="App 1" />
           </div>
           : null}
-        {show2 || path === '/app2' ?
+        {show2 ?
           <div className="card">
-            <TodoList title="App 2"/>
+            <TodoList title="App 2" />
           </div>
           : null}
       </div>
